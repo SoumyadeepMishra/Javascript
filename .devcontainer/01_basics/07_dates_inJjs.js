9f807a2c8ccd5e9a03cca5e33581daa8651fbdd0
SoumyadeepMishra <soumyadeepmishra14@gmail.com>
2025-07-28 18:38:51 +0000

    dates and time basics

---
+++ .devcontainer/01_basics/07_dates_inJjs.js
@@ -0,0 +1,12 @@
+// dates
+
+let myDate = new Date()
+console.log(myDate.toDateString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date("2023-01-14")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+//console.log(myCreatedDate.getTime());
+
