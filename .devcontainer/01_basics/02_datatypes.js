fed4beae6fa0247709479d22edd924c4ea838cbf
SoumyadeepMishra <soumyadeepmishra14@gmail.com>
2025-07-28 13:37:27 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+ "use strict"; // treat all js code as newer version
+ //alert(3 + 3) // we are using node js not browser
+ let name = "hitesh"
+ let age = 18
+ let isLoggedIn = false
+ // null is a stand alone value
+ // undefined => like let state
+ //symbol => unique
+
+ // object
+ console.log(typeof null)// object
