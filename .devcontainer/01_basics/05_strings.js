92f24f07cdcee1a2fa6169b06d26619a16257cdd
SoumyadeepMishra <soumyadeepmishra14@gmail.com>
2025-07-28 16:48:26 +0000

    strings

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,21 @@
+const name = "hitesh"
+const repoCount = 50
+
+//console.log(name + repoCount + " value"); 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("jndfj")
+
+console.log(gameName.charAt(2));
+
+const newString = gameName.substring(0,4);
+
+const newStringOne = "   hitesh   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
