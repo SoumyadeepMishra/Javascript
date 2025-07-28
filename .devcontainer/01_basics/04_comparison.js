29c8f8c27f3934d70d3a638f9357a19917df31c7
SoumyadeepMishra <soumyadeepmishra14@gmail.com>
2025-07-28 15:19:05 +0000

    datatypes and comoparisons

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,5 @@
+//console.log(2 > 1) // true
+
+console.log("2" > 1);
+
+// === strictly check checking datatypes also 
---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,14 @@
+// primitive
+
+// 7 types : string, Number, Boolean, null, undefined, symbol, BigInt
+
+const score = 100
+
+const id = Symbol('123')
+const anotherId = Symbol('123');
+
+console.log(id === anotherId);
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
