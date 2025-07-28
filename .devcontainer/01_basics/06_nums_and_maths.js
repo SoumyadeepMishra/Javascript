0a73ed98e3e3c4507bf4dc212f7e9f763606bcea
SoumyadeepMishra <soumyadeepmishra14@gmail.com>
2025-07-28 18:15:35 +0000

    maths and numbers

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,22 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+// ++++++++++Maths+++++++
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.8));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max  = 20
+
+console.log(Math.floor((Math.random() * (max - min +1)) ) + min)
