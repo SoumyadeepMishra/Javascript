1476df6a74acd4d1567cb3565d42885069e5344f
SoumyadeepMishra <soumyadeepmishra14@gmail.com>
2025-07-28 19:42:47 +0000

    basic array more to learn

---
+++ 02_basics/01_array.js
@@ -0,0 +1,19 @@
+// array
+
+const myArr = [1,2,3]
+console.log(myArr[0])
+
+const numbers = new Array(4,5,6,7,7,8)
+console.log(numbers)
+
+myArr.push(6)
+myArr.pop() // pop the last value of array
+
+myArr.unshift(9) // inserts the element into the first in array
+myArr.shift() // shifts back the array
+
+const newArr = myArr.join() // add the all the elements of the array into a string
+
+
+////// slice splice
+// splice manipulates the original array itself 
