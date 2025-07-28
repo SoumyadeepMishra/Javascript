eadb367322ecbc3826bf3b86b8372a81649a03f4
SoumyadeepMishra <soumyadeepmishra14@gmail.com>
2025-07-28 14:57:14 +0000

    conversion operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -12,3 +12,13 @@ console.log(typeof valueInNumber);
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 console.log(booleanIsLoggedIn);
+let value = 3;
+let negValue = - value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2;
+console.log(str3);
+
